616d6e90cadd67e4060b382d9a37b8a2c14989be
Ethan L. Mines <elmines@users.noreply.github.com>
2026-04-02 10:12:27 -0400

    Update gpt example to take string inputs

--- gpt_example.py
+++ gpt_example.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 from typing import Sequence, List
 
 from vllm import LLM, SamplingParams
@@ -16,7 +17,16 @@ from openai_harmony import (
 from utils import list_rfind
 
 class GptWrapper:
-    def __init__(self, hf_model: str | LLM):
+    def __init__(self,
+                 hf_model: str):
+        self.encoding = load_harmony_encoding(HarmonyEncodingName.HARMONY_GPT_OSS)
+        self.prompt_token_ids = self.encoding.render_conversation_for_completion(Conversation.from_messages([
+            Message.from_role_and_content(Role.SYSTEM, SystemContent.new()),
+            Message.from_role_and_content(
+                Role.DEVELOPER,
+                DeveloperContent.new().with_instructions("You are a helpful assistant."))
+            ]), Role.ASSISTANT)
+
         self.llm = LLM(model=hf_model, reasoning_parser="openai_gptoss")
         self.tokenizer = self.llm.get_tokenizer()
 
@@ -26,20 +36,21 @@ class GptWrapper:
         self.message_id = added_vocab['<|message|>']
         self.final_id = self.tokenizer.convert_tokens_to_ids('final')
 
-        self.encoding = load_harmony_encoding(HarmonyEncodingName.HARMONY_GPT_OSS)
-        self.sampling_params = SamplingParams(max_tokens=384,
-                                              temperature=1,
+    def __call__(self, samples: str | Sequence[str], max_tokens: int = 512, temperature: float = 1.) -> str | List[str]:
+        if isinstance(samples, str):
+            samples = [samples]
+
+        sampling_params = SamplingParams(max_tokens=max_tokens,
+                                              temperature=temperature,
                                               stop_token_ids=self.encoding.stop_tokens_for_assistant_actions())
-    
-    def __call__(self, convos: Conversation | Sequence[Conversation]) -> str | List[str]:
-        if isinstance(convos, Conversation):
-            convos = [convos]
 
         batch_prompts = [
-            {"prompt_token_ids": self.encoding.render_conversation_for_completion(c, Role.ASSISTANT)
-            } for c in convos
+            {"prompt_token_ids": self.prompt_token_ids + self.encoding.render_conversation_for_completion(
+                Conversation.from_messages([Message.from_role_and_content(Role.USER, s)]),
+                Role.ASSISTANT)
+            } for s in samples
         ]
-        batch_outputs = self.llm.generate(batch_prompts, sampling_params=self.sampling_params)
+        batch_outputs = self.llm.generate(batch_prompts, sampling_params=sampling_params)
 
         text_outputs = []
         for output in batch_outputs:
@@ -59,3 +70,28 @@ class GptWrapper:
         if len(text_outputs) == 1:
             return text_outputs[0]
         return text_outputs
+
+if __name__ == "__main__":
+    model_path = sys.argv[1] if len(sys.argv) > 1 else "openai/gpt-oss-20b"
+
+    # Run Demo
+    gpt = GptWrapper(hf_model=model_path)
+
+    short_question = "How would you bring peace to the Middle East?"
+    long_question = \
+    """
+    What is the opinion of the text below toward the target \"Pineapple on Pizza\"?
+    Please answer with only a single word, one of \"favor\", \"against\", or \"neutral\":
+
+    Tangy and savory do not mix.
+    """
+
+    samples = [short_question, long_question]
+
+    outputs = gpt(samples)
+
+    print(f"USER: {short_question}")
+    print(f"GPT: {outputs[0]}")
+
+    print(f"USER: {long_question}")
+    print(f"GPT: {outputs[1]}")
